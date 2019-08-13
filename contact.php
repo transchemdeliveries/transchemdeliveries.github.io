56dab3db49b8552c0571746fb358e103e2ac8ebe
Sahil Muthoo <sahil.muthoo@gmail.com>
2019-08-14 00:06:27 +0100

    stuff

--- contact.php
+++ contact.php
@@ -7,17 +7,17 @@ if(isset($_POST['SUBMIT'])){
 	if($capt->check_result()){
 		$bln_captcha = true;
 	}
-	
+
 	if($bln_captcha == true){
-		$to = 'sanjai.jalla@transchemdeliveries.com';
-		$to1 = 'neetu.sohal@transchemdeliveries.com';
+		$to = 'test@example.com';
+		$to1 = 'test@example.com';
 		$sub = "Query From Transchem Deliveries";
 		$from = trim($_POST['email']);
 		$comments = trim($_POST['comments']);
 		$uname = trim($_POST['uname']);
 		$company = trim($_POST['company']);
 		$phone = trim($_POST['phone']);
-		
+
 		$msg = "<table align='center' border='0' cellpadding='0' cellspacing='0' width='550' >
 					<tr>
 						<td><font style='font-family:Arial, Helvetica, sans-serif;font-size:14px;font-weight:bold;color:#000000'>Hi, </font></td>
@@ -29,15 +29,15 @@ if(isset($_POST['SUBMIT'])){
 					<tr><td height='10'></td></tr>
 					<tr>
 						<td><font style='font-family:Arial, Helvetica, sans-serif;font-size:12;'>Phone : ".$phone."</font></td>
-					</tr> 
+					</tr>
 					<tr><td height='10'></td></tr>
 					<tr>
 						<td><font style='font-family:Arial, Helvetica, sans-serif;font-size:12;'>Comments : ".$comments."</font></td>
 					</tr>
 				</table>";
-		
-		$headers = 	'MIME-Version: 1.0' . "\r\n" . 
-						'Content-type: text/html; charset=iso-8859-1' . "\r\n" . 
+
+		$headers = 	'MIME-Version: 1.0' . "\r\n" .
+						'Content-type: text/html; charset=iso-8859-1' . "\r\n" .
 						'From: ' . $uname . ' <' . $from . ">\r\n" .
 						'Reply-To: ' . $from . "\r\n" .
 						'X-Mailer: PHP/' . phpversion(); " \r\n";
